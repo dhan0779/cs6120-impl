df268ff6907734c6278a837d3d37eaadcbdea9d6
David Han <dhan@dhcp-vl2041-45053.eduroam.cornell.edu>
2025-01-30 12:53:04 -0500

    do basic cfg impl and reorg

--- l2/blocks.py
+++ l2/blocks.py
@@ -1,4 +1,4 @@
-import json, sys
+import json, sys, random
 
 TERMINATOR = 'br', 'jmp'
 
@@ -22,11 +22,41 @@ def form_blocks(func):
     
     return blocks
 
+def create_block_name_map(blocks):
+    block_map = {}
+    for block in blocks:
+        if 'label' in block[0]:
+            name = block[0]['label']
+        else:
+            name = 'block' + str(random.random())
+
+        block_map[name] = block
+    return block_map
+
+def form_cfg(blocks):
+    cfg = {} # label to list of labels
+    block_map = create_block_name_map(blocks)
+    for name, block in block_map.items():
+        last = block[-1]
+        if last['op'] in TERMINATOR:
+            if last['op'] == 'jmp' or last['op'] == 'br':
+                cfg[name] = [last['labels']]
+            else:
+                cfg[name] = []
+        else:
+            cfg[name] = []
+    
+    return cfg
+
+
 with open(sys.argv[1]) as f:
     program = json.load(f)
 
 
 for func in program['functions']:
-    print(form_blocks(func))
+    bb = form_blocks(func)
+    cfg = form_cfg(bb)
+    print(cfg)
+    # break
 
 
